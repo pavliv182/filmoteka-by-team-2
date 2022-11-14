f0c71b8bd4faf1a2bc24af8a91410597e9733b82
pavliv182 <97523241+pavliv182@users.noreply.github.com>
2022-11-14 11:31:16 +0000

    Deploying to gh-pages from @ pavliv182/filmoteka-by-team-2@d01001b0ba7d3d8dcdb88abb2ecb591a7d1802bb 🚀

---
+++ my-library.1a4b9e87.js
@@ -0,0 +1,2 @@
+var modal=document.getElementById("footerBtn"),btn=document.getElementById("teamBtn"),span=document.getElementsByClassName("footer-close")[0];btn.onclick=function(){modal.style.display="block"},span.onclick=function(){modal.style.display="none"},window.onclick=function(n){n.target==modal&&(modal.style.display="none")};
+//# sourceMappingURL=my-library.1a4b9e87.js.map
